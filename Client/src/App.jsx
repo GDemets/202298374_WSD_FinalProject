6595de1461c3b21e73d403f06de00e2175e45270
GDemets <gdemets@ensc.fr>
2025-12-02 17:54:52 +0900

    [ADD]-Page Register

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,25 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
 
-function App() {
+const App = () => {
+  const [page, setPage] = useState("Home");
+
+  const PageWrapper = ({ component: Component }) => {
+    return <Component setPage={setPage} />;
+  };
 
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<PageWrapper component={Home} />} />
+        <Route path="/register" element={<PageWrapper component={Register} />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
